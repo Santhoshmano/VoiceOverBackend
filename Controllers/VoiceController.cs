e7dd87c2d47e18575572f500b02642b900a2c14e
MAlexander <muthuraman.a@LP-PF229DZL>
2021-07-03 19:11:45 +0530

    Add project files.

---
+++ Controllers/VoiceController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace VoiceOver.Controllers
+{
+    public class VoiceController : Controller
+    {
+        [HttpPost("/voice"), DisableRequestSizeLimit]
+        public IActionResult Voice()
+        {
+            var file = Request.Form.Files[0];
+            //var formCollection = await Request.ReadFormAsync();
+            //var file = formCollection.Files.First();
+            Console.WriteLine(Request.Form);
+            var folderName = Path.Combine("Resources", "Voices");
+            var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+            Console.WriteLine(file);
+            Console.WriteLine(pathToSave);
+            if (file.Length > 0)
+            {
+                var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                var fullPath = Path.Combine(pathToSave, fileName);
+          
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+            
+        }
+
+        [HttpGet("/voice")]
+        public IActionResult GetVoice()
+        {
+            return Ok("Hello");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,86 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace VoiceOver
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("*");
+                                      builder.AllowAnyHeader();
+                                      builder.AllowAnyMethod();
+                                                          
+                                  });
+            });
+
+            services.Configure<FormOptions>(o => {
+                o.ValueLengthLimit = int.MaxValue;
+                o.MultipartBodyLengthLimit = int.MaxValue;
+                o.MemoryBufferThreshold = int.MaxValue;
+                o.ValueCountLimit = int.MaxValue;
+            });
+
+            services.AddControllers();
+
+           
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseAuthorization();
+
+            var wsOptions = new WebSocketOptions()
+            {
+                KeepAliveInterval = TimeSpan.FromSeconds(120),
+                ReceiveBufferSize = 4 * 1024
+            };
+            app.UseWebSockets(wsOptions);
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+           
+           
+        }
+    }
+}
